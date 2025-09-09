057aa8abb63c955939898b95262a875dca088f73
pulidu <godagepulindu@gmail.com>
2025-09-10 02:00:06 +0530

    1st Commit (CreatePackages)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.orm_final_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.orm_final_project to javafx.fxml;
+    exports lk.ijse.orm_final_project;
+}
